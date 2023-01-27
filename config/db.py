14188b93d78ef4e7fe086cdc541bd9edfbb5980f
Sanjay Kumar <sk.nitw08@gmail.com>
2023-01-28 02:30:06 +0530

    config changes

--- config/db.py
+++ config/db.py
@@ -1,12 +1,17 @@
 from sqlalchemy import create_engine,MetaData
-# Orig:
-# engine = create_engine('mysql+pymysql://root:password@localhost:3306/text')
-# engine = create_engine('mysql+pymysql://admin01:Magicevents01@azdbmysqlpoc.mysql.database.azure.com:3306/test')
+import os
+import urllib
 
 # conn = engine.connect()
 
+host_server = os.environ.get('host_server')
+db_server_port = urllib.parse.quote_plus(str(os.environ.get('db_server_port')))
+database_name = os.environ.get('database_name')
+db_username = urllib.parse.quote_plus(str(os.environ.get('db_username')))
+db_password = urllib.parse.quote_plus(str(os.environ.get('db_password')))
+
 connect_args={'ssl':{'fake_flag_to_enable_tls': True}}
-connect_string = 'mysql+pymysql://{}:{}@{}/{}'.format("admin01","Magicevents01","azdbmysqlpoc.mysql.database.azure.com:3306","test")
+connect_string = 'mysql+pymysql://{}:{}@{}:{}/{}'.format(db_username,db_password,host_server,db_server_port,database_name)
 engine = create_engine(connect_string,connect_args=connect_args)
 conn = engine.connect()
 
